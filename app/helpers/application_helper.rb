2217ac5ab813d63521ddb6f048553ee5a6f8f7d3
Matthew Kennedy <m.kennedy@me.com>
2024-03-04 21:36:18 +0000

    move to helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,19 @@
 module ApplicationHelper
+
+  def component_id(name)
+    "#{Rails.application.class.module_parent_name.downcase}--#{name}"
+  end
+
+  def component_css(name, &block)
+    return if instance_variable_get(component_instance_var(name))
+
+    instance_variable_set(component_instance_var(name), true)
+    content_for :view_component_head do
+      yield
+    end
+  end
+
+  def component_instance_var(name)
+    "@#{Rails.application.class.module_parent_name.downcase}_#{name}_erbcv"
+  end
 end
