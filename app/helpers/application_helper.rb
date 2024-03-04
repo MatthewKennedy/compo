f8a6eabff2bea8659e05fc7fa44643d0a8b0e1a8
Matthew Kennedy <m.kennedy@me.com>
2024-03-04 22:37:58 +0000

    thrash out some helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,19 +1,34 @@
 module ApplicationHelper
-
-  def component_id(name)
-    "#{Rails.application.class.module_parent_name.downcase}--#{name}"
+  def erbvc_id
+    "#{Rails.application.class.module_parent_name.downcase}--#{erbvc_name}"
   end
 
-  def component_css(name, &block)
-    return if instance_variable_get(component_instance_var(name))
+  def erbvc_css(&block)
+    return if instance_variable_get(erbvc_var)
 
-    instance_variable_set(component_instance_var(name), true)
-    content_for :view_component_head do
+    instance_variable_set(erbvc_var, true)
+    content_for :erb_view_component_head_css do
       yield
     end
   end
 
-  def component_instance_var(name)
-    "@#{Rails.application.class.module_parent_name.downcase}_#{name}_erbcv"
+  def erbvc_var
+    "@#{Rails.application.class.module_parent_name.downcase}_#{erbvc_name}_erbcv"
+  end
+
+  def erbvc_html(theme: nil, css_vars: nil, &block)
+    content_tag(:div, class: "#{erbvc_id} #{theme}", style: "css_vars", &block)
+  end
+
+  def erbvc_name
+    caller_path.split(".").first.split("/").last.sub(/^_/, "")
+  end
+
+  def caller_path
+    caller_locations(2, 1).first.path
+  end
+
+  def erbvc_container_class
+    "div.#{erbvc_id}"
   end
 end
