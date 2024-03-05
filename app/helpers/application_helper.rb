348d36fcc147b7f37c88df5ee0964f87a66dba47
Matthew Kennedy <m.kennedy@me.com>
2024-03-05 14:15:54 +0000

    chore: add comments

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,7 @@
 module ApplicationHelper
+  # Set an identifier by using the app
+  # and component name with the erbv prefix.
+  #
   def erbv_id
     "erbv--#{Rails.application.class.module_parent_name.downcase}--#{erbv_name}"
   end
@@ -7,10 +10,17 @@ module ApplicationHelper
     erbv_id
   end
 
-  def erbv_css(&block)
+  def erbv_css(skip_auto_css_container: false, container_type: "inline-size", &block)
     return if instance_variable_get(erbv_var("css"))
 
     instance_variable_set(erbv_var("css"), true)
+
+    unless skip_auto_css_container
+      content_for(:erbv_css) do
+        content_tag(:style, "#{erbv_component_base_selector} { container: #{erbv_css_container_query_id} / #{container_type}; }")
+      end
+    end
+
     content_for(:erbv_css, &block)
   end
 
@@ -22,20 +32,30 @@ module ApplicationHelper
   end
 
   def erbv_var(postfix)
-    "@#{Rails.application.class.module_parent_name.downcase}_#{erbv_name}_erbv_#{postfix}"
+    "@erbv_#{Rails.application.class.module_parent_name.downcase}_#{erbv_name}_#{postfix}"
   end
 
-  def erbv_html(theme: nil, css_vars: nil, &block)
-    content_tag(:div, class: "#{erbv_id} #{theme}", style: "container: #{erbv_css_container_query_id} / inline-size;", &block)
+  # Creates the container block level element specifically with
+  # one fixed class and an optional theme name class.
+  #
+  # You can pass inline styles, to style individual components independent.
+  #
+  def erbv_html(theme: nil, style: nil, &block)
+    theme_class_name = "#{erbv_id} #{theme}".strip
+
+    content_tag(:div, class: theme_class_name, style:, &block)
   end
 
+  # Set the component name by finding the filename from the erb file
+  #
   def erbv_name
     caller_locations.each do |location|
-      return location.path.split(".").first.split("/").last.sub(/^_/, "") if location&.path&.ends_with?(".erb")
+      return location.path.split(".").first.split("/").last.sub(/^_/, "") if location&.path&.ends_with?(".html.erb")
     end
   end
 
   # Defines a top level selector for the component
+  #
   def erbv_component_base_selector
     "div.#{erbv_id}"
   end
